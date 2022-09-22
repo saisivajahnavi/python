50a12c85184e458cf3bdc14dd4b52592207a4550
saisivajahnavi <113570511+saisivajahnavi@users.noreply.github.com>
2022-09-22 10:30:54 +0530

    Add files via upload
    enter the month:mar
    enter the date:21
    summer
    >>> 
    == RESTART: C:/Users/p.sai siva jahnavi/OneDrive/Desktop/py/day 2 - seasons.py =
    enter the month:jul
    enter the date:29
    spring
    >>> 
    == RESTART: C:/Users/p.sai siva jahnavi/OneDrive/Desktop/py/day 2 - seasons.py =
    enter the month:sep
    enter the date:5
    fall
    >>> 
    == RESTART: C:/Users/p.sai siva jahnavi/OneDrive/Desktop/py/day 2 - seasons.py =
    enter the month:dec
    enter the date:30
    fall
    >>> 
    == RESTART: C:/Users/p.sai siva jahnavi/OneDrive/Desktop/py/day 2 - seasons.py =
    enter the month:mar
    enter the date:12
    winter
    
    enter the month:jun
    enter the date:27
    spring

---
+++ day 2 - seasons.py
@@ -0,0 +1,32 @@
+m=input("enter the month:")
+d=int(input("enter the date:"))
+if(d>1 and d<=31):
+    if(m=='apr' or m=='may'):
+        print("summer")
+    elif(m=='jun' and d<=20):
+        print("summer")
+    elif(m=='mar' and d>=20):
+        print("summer")
+    elif(m=='jul' or m=='aug'):
+        print("spring")
+    elif(m=='jun' and d<=27):
+        print("spring")
+    elif(m=='sep' and d>=21):
+        print("spring")
+    elif(m=='oct' or m=='nov'):
+        print("fall")
+    elif(m=='sep' and d<=22):
+        print("fall")
+    elif(m=='dec' and d>=21):
+        print("fall")
+    elif(m=='feb' or m=='jan'):
+        print("winter")
+    elif(m=='mar' and d<=19):
+        print("winter")
+    elif(m=='dec' and d>=20):
+        print("winter")
+else:
+    print("invaliddd")
+    
+    
+      
